d6030e98536e54c587c046ad20c174f003352286
Muhammad Haris <m.haris425@gmail.com>
2026-08-02 23:15:35 -0400

    feat: create a page for home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+
+
+import Hero from "../components/Hero";
+import HistorySection from "../components/HistorySection";
+
+function Home(){
+    return(
+        <>
+            <Hero/>
+            <HistorySection/>
+        </>
+    )
+}
+
+export default Home;
