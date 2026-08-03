eb31b60fd741e9f553c1b2dc8ca09244601eeb50
Muhammad Haris <m.haris425@gmail.com>
2026-08-02 23:18:33 -0400

    feat: add component for the Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+//import React from "react";
+import atlas from "../assets/images/atlas-plain2.png"
+
+function Hero(){
+    return(
+        <>
+            <section className="hero">
+                <img src={atlas} alt="Hero Image"/>
+                <div className="container introduction">
+                    <h1>The Computing Atlas</h1>
+                    <p>
+                    Explore the people, ideas, and breakthroughs that shaped computer science. From pioneering algorithms 
+                    and operating systems to the innovators behind them, discover the history, concepts, and stories that 
+                    continue to influence modern computing.
+                    </p>
+                </div>
+                
+            </section>
+        </>
+    )
+}
+
+export default Hero;
