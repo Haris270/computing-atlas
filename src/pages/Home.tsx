178c27929033da3ec754e939b3e47ee37a09b11f
Muhammad Haris <m.haris425@gmail.com>
2026-08-03 20:54:00 -0400

    refactor: integrate Algorithm section on main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@
 
 import Hero from "../components/Hero";
 import HistorySection from "../components/HistorySection";
+import AlgorithmSection from "../components/AlgorithmSection";
 
 function Home(){
     return(
         <>
             <Hero/>
             <HistorySection/>
+            <AlgorithmSection/>
         </>
     )
 }
