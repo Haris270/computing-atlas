925bb7c50ca0814a3ac4cd8bdb37aae0163ac84a
Muhammad Haris <m.haris425@gmail.com>
2026-08-15 23:20:14 -0400

    feat: add lazy loading to Home page's sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,18 @@
 import Hero from "../components/Hero";
 import HistorySection from "../components/HistorySection";
 import AlgorithmSection from "../components/AlgorithmSection";
+import LazySection from "../components/LazySection";
 
 function Home(){
     return(
         <>
             <Hero/>
-            <HistorySection/>
-            <AlgorithmSection/>
+            <LazySection>
+                <HistorySection/>
+            </LazySection>
+            <LazySection>
+                <AlgorithmSection/>
+            </LazySection>
         </>
     )
 }
